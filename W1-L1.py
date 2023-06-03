3b4f8b3f37a9046aba830bf77377766e03e2e951
extrastufff054 <extrastuff054@gmail.com>
2023-06-03 23:27:13 +0530

    03-06-2023

---
+++ W1-L1.py
@@ -0,0 +1,16 @@
+print("Hello world!!")
+print("This is the second line.")
+
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
+print("******")
+print("*******")
+
+print("I don't know what to type here.")
+
+print("  *")
+print(" **")
+print("***")
---
+++ W1-L2.py
@@ -0,0 +1,20 @@
+# Multiple statements can be printed in a single print statement by comma seperating the values
+print("Hello world!!", "This is printing two different statements using a single print function\n")
+
+#Printing numbers
+print(10)
+print(13.1234)
+
+print("Hello world!!", 10, 123.23)
+
+## Common errors 
+# Syntax of print : print("string") OR print(variable)
+print("Hello world!!")
+
+# The print statement is strict with different type of brackets as compared to quotes
+# print('Hello world!!") # This will throw an error
+print("Hello world!!")  # This will work fine
+# print ["Hello world!!"] # This will throw an error
+# print{"Hello world!!"} # This will throw an error
+# print("Hello world!!) # This will throw an error
+
